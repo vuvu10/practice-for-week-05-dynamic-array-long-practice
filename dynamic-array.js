5892d6d28d560f0dcda78e824334f1e805b7ee7d
dcsiona@gmail.com <dcsiona@gmail.com>
2024-03-16 16:07:36 -0400

    another commit

--- dynamic-array.js
+++ dynamic-array.js
@@ -39,22 +39,49 @@ class DynamicArray {
   shift() {
 
     // Your code here
+    if (this.length === 0) return undefined;
+    const removedElement = this.data[0];
+    for (let i = 1; i < this.length; i++) {
+      this.data[i - 1] = this.data[i];
+    }
+    delete this.data[this.length - 1];
+    this.length--;
+    return removedElement;
 
   }
 
   unshift(val) {
 
     // Your code here
+    if (this.length === this.capacity) {
+      this.resize();
+    }
+    for (let i = this.length - 1; i >= 0; i--) {
+      this.data[i + 1] = this.data[i];
+    }
+    this.data[0] = val;
+    this.length++;
   }
 
   indexOf(val) {
 
     // Your code here
+    for (let i = 0; i < this.length; i++) {
+      if (this.data[i] === val) return i;
+    }
+    return -1;
   }
 
   resize() {
 
     // Your code here
+    const newCapacity = this.capacity * 2;
+    const newData = new Array(newCapacity);
+    for (let i = 0; i < this.length; i++) {
+      newData[i] = this.data[i];
+    }
+    this.data = newData;
+    this.capacity = newCapacity;
   }
 
 }
